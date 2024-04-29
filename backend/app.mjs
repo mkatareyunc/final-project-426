37f937de75eede3a3254cd4e9a263e89ff59ae46
kvgzz <kgomez@unc.edu>
2024-04-28 20:10:26 -0400

    Added changes to api route, changed gitignore file

--- backend/app.mjs
+++ backend/app.mjs
@@ -10,23 +10,22 @@ app.use(bodyParser.json());
 
 app.use(cors())
 
-const api_key = process.env.api_key;
+const access_token = await getAccessToken();
 
 const port = 3000;
 
-app.get('/points', async(req, res) => {
-    try{
-        let response = await axios.get('https://') // Add API URL
-        res.json();
-    }catch(e){
-        res.status(400).send('Error Retrieving Data');
-    }
-});
-
 app.get('/tours', async(req, res) => {
     try{
-        let response = await axios.get('') // Add API URL
-        res.json();
+        let longitude = req.query.longitude;
+        let latitude = req.query.latitude;
+        let radius = req.query.radius;
+        let response = await axios.get(`https://test.api.amadeus.com/v1/shopping/activities?latitude=${latitude}&longitude=${longitude}&radius=${radius}`, {
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
+        });
+        let responseData = response.data; 
+        res.json(responseData);
     }catch(e){
         res.status(400).send('Error Retrieving Data');
     }
@@ -36,6 +35,24 @@ app.post('/user', async(req, res) => {
     
 });
 
+//This function is to retrieve user access token from Amadeus API
+async function getAccessToken() {
+    try {
+        const data = new URLSearchParams({
+            grant_type: 'client_credentials',
+            client_id: "RE3Zx0WximAXUG4rbOwIsar2RhH2Di8Y",
+            client_secret: "ur0G084QS8GVd2lm",
+        });
+        let response = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', data, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        });
+        return response.data.access_token;
+    } catch (e) {
+        return null;
+    }
+}
 
 app.listen(port, () => {
     console.log(`Server is running on https://localhost:${port}`);
