a9cee18a60f93dc495d2098972e8bca3339a2d4d
Kevin Gomez <89496614+kvgzz@users.noreply.github.com>
2024-04-28 21:25:05 -0400

    Added changes to api route, changed gitignore file (#7)
    * Added changes to api route, changed gitignore file
    
    * Fixing main, collected all current code in all of our files in order to push to main

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,12 +1,59 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { db } from './db.mjs';
+import axios from 'axios';
+import { db } from 'backend/db.mjs';
+
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+
+app.use(cors())
+
+const access_token = await getAccessToken();
+
+const port = 3000;
+
+app.get('/tours', async(req, res) => {
+    try{
+        let longitude = req.query.longitude;
+        let latitude = req.query.latitude;
+        let radius = req.query.radius;
+        let response = await axios.get(`https://test.api.amadeus.com/v1/shopping/activities?latitude=${latitude}&longitude=${longitude}&radius=${radius}`, {
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
+        });
+        let responseData = response.data; 
+        res.json(responseData);
+    }catch(e){
+        res.status(400).send('Error Retrieving Data');
+    }
+});
+
+app.post('/user', async(req, res) => {
+    
+});
+
+//This function is to retrieve user access token from Amadeus API
+async function getAccessToken() {
+    try {
+        const data = new URLSearchParams({
+            grant_type: 'client_credentials',
+            client_id: "RE3Zx0WximAXUG4rbOwIsar2RhH2Di8Y",
+            client_secret: "ur0G084QS8GVd2lm",
+        });
+        let response = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', data, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        });
+        return response.data.access_token;
+    } catch (e) {
+        return null;
+    }
+}
 
 // Registration endpoint
 app.post('/register', async (req, res) => {
@@ -50,7 +97,6 @@ app.post('/login', async (req, res) => {
     }
 });
 
-const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on https://localhost:${port}`);
 });
