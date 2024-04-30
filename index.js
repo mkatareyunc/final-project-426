4bf47d7f2b339dd8feebce903ce0987ef889716a
Kevin Gomez <89496614+kvgzz@users.noreply.github.com>
2024-04-30 17:52:14 -0400

    fullstack v.0.1 (#15)
    * april 28th
    
    * final commit of apr28
    
    * updated frontend
    
    * changed the template before selection
    
    * version 2 of fullstack
    
    * Changed setting link, linked favorite page
    
    * Potentially finished heart button, only for barcelona
    
    * added heart implementation, user file changes, changed password input type
    
    * Co-authored-by: Pauline Dang <PaulineDangUNC@users.noreply.github.com>
    
    * Fixed colors, pictures for attractions
    
    ---------
    
    Co-authored-by: Morgan Kennedy <morgangk@ad.unc.edu>

--- index.js
+++ index.js
@@ -21,11 +21,16 @@ loginBtn.addEventListener("click", () => {
         if (response.ok) {
             window.location.assign("page2.html");
             // Redirect or show success message
-            console.log("Login successful");
+            return response.json();
         } else {
             // Handle login failure
             console.error("Login failed");
         }
+    })
+    .then(data => {
+        //store userid in local storage
+        localStorage.setItem('userId', data.userId);
+        localStorage.setItem('username', data.username);
     })
     .catch(error => {
         console.error("Error:", error);
